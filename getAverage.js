ea3b153412a17697108e8fe13ff66ad007d08028
Sridatta <sridatta.pasumarthy@oracle.com>
2020-10-12 17:29:12 +0530

    Updated scrapes

--- getAverage.js
+++ getAverage.js
@@ -5,13 +5,14 @@ import {
   groupReducer,
   retrieveTagsByTitle,
   removeComicStripLinks,
-  retrieveGroupedTagsByTagsArray
+  retrieveGroupedTagsByTagsArray,
 } from "./utils";
 
 var fs = require("fs");
 
 export const getData = async () => {
   let data = [];
+  console.log("SCRAPING HIGH AVERAGE PLAYERS");
   const html1 = await getHTML(
     `http://stats.espncricinfo.com/ci/content/records/282910.html`
   );
@@ -26,19 +27,12 @@ export const getData = async () => {
         .find("a")
         .attr("href")
         .match(/[0-9]*/g)
-        .filter(a => a)[0],
-      name: $(el)
-        .find("td:nth-child(1)")
-        .find("a")
-        .text(),
-      country: [
-        $(el)
-          .find("td:nth-child(1)")
-          .text()
-      ].map(a => {
+        .filter((a) => a)[0],
+      name: $(el).find("td:nth-child(1)").find("a").text(),
+      country: [$(el).find("td:nth-child(1)").text()].map((a) => {
         const reg = a.match(/\([A-b]*\)/g) || a.match(/\([A-b]*\/[A-b]*\)/g);
         return reg && reg[0];
-      })[0]
+      })[0],
     }))
     .toArray();
 
@@ -51,25 +45,41 @@ export const getData = async () => {
     const details = $(
       "#ciHomeContentlhs > div.pnl650M > table:nth-child(5) > tbody > tr"
     )
-      .map((i, el) =>
-        $(el)
-          .find("td:nth-child(6)")
-          .text()
-      )
+      .map((i, el) => $(el).find("td:nth-child(6)").text())
       .toArray()
       .map((average, matchNo) => ({
         playerName: players[playerCount].name,
         playerId: Number(players[playerCount].playerId),
         country: players[playerCount].country,
-        height: 80,
+        height: 56,
         average: Number(average),
-        matchNo: Number(matchNo) + 1
+        matchNo: Number(matchNo) + 1,
       }));
     data.push(...details);
   }
   const dataStr = JSON.stringify(data);
-  fs.writeFile("./data.json", dataStr, err => {
+  fs.writeFile("./data.json", dataStr, (err) => {
     if (err) console.error(err);
   });
   return data;
+};
+
+export const processData = () => {
+  const masterData = JSON.parse(fs.readFileSync("data.json", "utf8"));
+  const masterResult = Array.from({ length: 73 }).reduce((acc, elem, index) => {
+    const filteredAndSorted = masterData
+      .filter((entry) => entry.matchNo === index + 1)
+      .map((entry) => ({
+        ...entry,
+        country: entry.country.substr(1, entry.country.length - 2),
+      }));
+    filteredAndSorted.sort((a1, a2) => a2.average - a1.average);
+    return {
+      ...acc,
+      [index + 1]: filteredAndSorted.slice(0, 10),
+    };
+  }, {});
+  fs.writeFile("./result.json", JSON.stringify(masterResult), (err) => {
+    if (err) console.error(err);
+  });
 };
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { getCount } from "./ipl";
 // import { getData } from "./baeldung";
-import { getData } from "./getAverage";
+import { getData, processData } from "./getAverage";
 
 async function invokeFunction() {
-  await getData();
+  // await getData();
+  await processData();
 }
 
 invokeFunction();
